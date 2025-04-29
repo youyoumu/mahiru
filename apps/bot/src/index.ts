f7bd225e5a66cb2dedffdb2677befe6a3834bc36
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-04-29 17:56:29 +0700

    Colocate event

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -1,23 +1,12 @@
 // Require the necessary discord.js classes
-import {
-  Client,
-  Collection,
-  Events,
-  GatewayIntentBits,
-  MessageFlags,
-} from "discord.js";
+import { Client, Collection, GatewayIntentBits } from "discord.js";
 import { env } from "./env";
 import ping from "./commands/utility/ping";
+import interactionCreate from "./events/interactionCreate";
+import ready from "./events/ready";
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
-client.once(Events.ClientReady, (readyClient) => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
 
 // Log in to Discord with your client's token
 client.login(env.DISCORD_TOKEN);
@@ -33,6 +22,7 @@ declare module "discord.js" {
 client.commands = commands;
 
 const commandArray = [ping];
+const events = [interactionCreate, ready];
 
 for (const command of commandArray) {
   if ("data" in command && "execute" in command) {
@@ -44,33 +34,12 @@ for (const command of commandArray) {
   }
 }
 
-client.on(Events.InteractionCreate, async (interaction) => {
-  console.log(interaction);
-  if (!interaction.isChatInputCommand()) return;
-
-  const command = interaction.client.commands.get(
-    interaction.commandName,
-  ) as typeof ping;
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
+for (const event of events) {
+  if (event.once) {
+    client.once(event.name as any, (interaction) => event.execute(interaction));
+  } else {
+    client.on(event.name as any, (interaction) => {
+      event.execute(interaction);
+    });
   }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({
-        content: "There was an error while executing this command!",
-        flags: MessageFlags.Ephemeral,
-      });
-    } else {
-      await interaction.reply({
-        content: "There was an error while executing this command!",
-        flags: MessageFlags.Ephemeral,
-      });
-    }
-  }
-});
+}
