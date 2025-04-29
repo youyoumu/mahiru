ef0cb1f3c52249b0ff45dd2c189fbaabf288bbdb
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-04-29 21:58:17 +0700

    Add backend apps

---
+++ apps/backend/src/env.ts
@@ -0,0 +1,19 @@
+import { createEnv } from "@t3-oss/env-core";
+import { pipe, string, transform } from "valibot";
+
+export const env = createEnv({
+  server: {
+    PORT: pipe(
+      string(),
+      transform((input) => Number(input)),
+    ),
+  },
+
+  clientPrefix: "PUBLIC_",
+
+  client: {},
+
+  runtimeEnv: process.env,
+
+  emptyStringAsUndefined: true,
+});
---
+++ apps/backend/src/index.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import { serve } from "@hono/node-server";
+
+import app from "./app";
+import { env } from "./env";
+
+const port = env.PORT;
+console.log(`Server is running on port http://localhost:${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
