b74d066338b98f1df3e53eac28b1416b3bf7fb02
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-04-30 17:39:18 +0700

    Setup tailwind css

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -1,18 +1,23 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
+  plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact(),
+    tailwindcss(),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      "#": resolve(__dirname, "./src"),
     },
-  }
+  },
 });
