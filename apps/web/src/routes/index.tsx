0c6ffb181a09d253b5062a46cbd1a17d3c53b678
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-04-30 21:49:01 +0700

    Fix type erroor

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,7 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { client } from "@repo/utils";
-
 export const Route = createFileRoute("/")({
   component: () => <div></div>,
 });
