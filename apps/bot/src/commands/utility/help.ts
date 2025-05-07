8be188c04b85803549b631cc7244255b76d4f09b
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-05-07 17:14:11 +0700

    Add content to help command

--- apps/bot/src/commands/utility/help.ts
+++ apps/bot/src/commands/utility/help.ts
@@ -1,6 +1,7 @@
 import {
   ChatInputCommandInteraction,
   EmbedBuilder,
+  inlineCode,
   Message,
   SlashCommandBuilder,
 } from "discord.js";
@@ -11,15 +12,31 @@ export default {
     .setDescription("Explain all commands"),
   async execute(interaction: ChatInputCommandInteraction) {
     const embed = new EmbedBuilder()
-      .setDescription("Explain all commands")
+      .setTitle("Help")
+      .setColor("#fef3c6")
+      .setThumbnail(
+        "https://cdn.discordapp.com/avatars/1366671964500000778/555dfb9cf6265ae505041deeaac95b05",
+      )
       .addFields({
-        name: "asd",
-        value: "asd",
+        name: "<:azusarelaxed:1207544782952595508> ping",
+        value: "Replies with Pong",
       })
-
+      .addFields({
+        name: "<:azusarelaxed:1207544782952595508> login",
+        value: "Get link to login to Mahiru web app",
+      })
+      .addFields({
+        name: "<:azusarelaxed:1207544782952595508> meme",
+        value: `Manage your meme collections. Use ${inlineCode("/meme help")} for details`,
+      })
+      .addFields({
+        name: "<:azusarelaxed:1207544782952595508> prefix",
+        value: `Manage bot prefix for this server. Use ${inlineCode("/prefix help")} for details`,
+      })
       .setFooter({
-        text: "footer",
-      });
+        text: "Mahiru",
+      })
+      .setTimestamp();
 
     await interaction?.reply({ embeds: [embed] });
   },
