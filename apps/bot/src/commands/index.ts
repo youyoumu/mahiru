92abf059644dc6930d7c0b29af073faac2493892
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-05-07 15:02:03 +0700

    Create prefix command

--- apps/bot/src/commands/index.ts
+++ apps/bot/src/commands/index.ts
@@ -1,9 +1,11 @@
 import login from "./utility/login";
 import meme from "./utility/meme";
 import ping from "./utility/ping";
+import prefix from "./utility/prefix";
 
 export default {
   [ping.data.name]: ping,
   [meme.data.name]: meme,
   [login.data.name]: login,
+  [prefix.data.name]: prefix,
 };
---
+++ apps/bot/src/commands/utility/prefix.ts
@@ -0,0 +1,165 @@
+import {
+  ChatInputCommandInteraction,
+  codeBlock,
+  Message,
+  SlashCommandBuilder,
+} from "discord.js";
+import db, { schema } from "@repo/db";
+import { eq } from "drizzle-orm";
+
+const action = {
+  current: "current",
+  change: "change",
+} as const;
+
+const param = {
+  key: "key",
+  value: "value",
+  ["new-prefix"]: "new-prefix",
+};
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("prefix")
+    .setDescription("Manage discord prefix for this server")
+
+    .addSubcommand((subCommand) =>
+      subCommand
+        .setName(action.change)
+        .setDescription("Change bot prefix for this server")
+        .addStringOption((option) =>
+          option
+            .setName(param["new-prefix"])
+            .setDescription("New prefix")
+            .setMaxLength(2)
+            .setRequired(true),
+        ),
+    )
+
+    .addSubcommand((subCommand) =>
+      subCommand
+        .setName(action.current)
+        .setDescription("Show current bot prefix for this server"),
+    ),
+  async execute(interaction: ChatInputCommandInteraction) {
+    const selectedAction =
+      interaction.options.getSubcommand() as keyof typeof action;
+    const newPrefix = interaction.options.getString(param["new-prefix"]);
+    const discord_guild_id = interaction.guildId;
+
+    if (!discord_guild_id) return;
+
+    switch (selectedAction) {
+      case "change": {
+        if (newPrefix) {
+          return handleChange({
+            discord_guild_id,
+            prefix: newPrefix,
+            interaction,
+          });
+        }
+        return interaction.reply("⚠️ Invalid arguments");
+      }
+
+      case "current": {
+        return handleCurrent({
+          discord_guild_id,
+          interaction,
+        });
+      }
+    }
+
+    return interaction.reply("Something went wrong");
+  },
+  async prefixExecute({ message, args }: { message: Message; args: string[] }) {
+    const discord_guild_id = message.guildId;
+    if (!discord_guild_id) return;
+    const subCommand = args[0];
+
+    switch (subCommand) {
+      case action.change: {
+        const newPrefix = args[1];
+
+        if (newPrefix) {
+          return handleChange({
+            discord_guild_id,
+            prefix: newPrefix,
+            message,
+          });
+        }
+
+        if (message.channel.isSendable()) {
+          message.channel.send(codeBlock("change <new-prefix>"));
+        }
+        break;
+      }
+      case action.current: {
+        return handleCurrent({
+          discord_guild_id,
+          message,
+        });
+      }
+      default: {
+        return handleCurrent({
+          discord_guild_id,
+          message,
+        });
+      }
+    }
+  },
+};
+
+async function getGuildPrefix({
+  discord_guild_id,
+}: {
+  discord_guild_id: string;
+}) {
+  return await db.query.prefixes.findFirst({
+    where(fields, operators) {
+      return operators.eq(fields.discord_guild_id, discord_guild_id);
+    },
+  });
+}
+
+async function handleChange({
+  discord_guild_id,
+  prefix,
+  interaction,
+  message,
+}: {
+  discord_guild_id: string;
+  prefix: string;
+  interaction?: ChatInputCommandInteraction;
+  message?: Message;
+}) {
+  const guildPrefix = await getGuildPrefix({ discord_guild_id });
+
+  if (guildPrefix) {
+    db.update(schema.prefixes)
+      .set({ prefix: prefix })
+      .where(eq(schema.prefixes.id, guildPrefix.id));
+  } else {
+    db.insert(schema.prefixes).values({
+      discord_guild_id: discord_guild_id,
+      prefix: prefix,
+    });
+  }
+
+  interaction?.reply(codeBlock(prefix));
+  if (message?.channel.isSendable()) message.channel.send(codeBlock(prefix));
+}
+
+async function handleCurrent({
+  discord_guild_id,
+  interaction,
+  message,
+}: {
+  discord_guild_id: string;
+  interaction?: ChatInputCommandInteraction;
+  message?: Message;
+}) {
+  const prefix = (await getGuildPrefix({ discord_guild_id }))?.prefix ?? "!";
+
+  interaction?.reply(codeBlock(prefix));
+  if (message?.channel.isSendable()) message.channel.send(codeBlock(prefix));
+}
