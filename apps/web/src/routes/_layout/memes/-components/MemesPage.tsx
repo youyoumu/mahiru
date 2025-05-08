f68507241d335e371f02dd2f7a6ba58092289256
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-05-08 23:32:39 +0700

    Create embed component

--- apps/web/src/routes/_layout/memes/-components/MemesPage.tsx
+++ apps/web/src/routes/_layout/memes/-components/MemesPage.tsx
@@ -11,12 +11,26 @@ import { Copy } from "lucide-react";
 import { toast } from "sonner";
 import type { ReactNode } from "react";
 import { useDiscordCdn } from "#/hooks/useProxy";
+import ReactPlayer from "react-player";
 
 export default function MemesPage() {
   const { data: memes = [] } = useMemes();
   console.log("DEBUG[342]: memes=", memes);
+  const url = new URL("https://imgur.com/msw9Al2/asdasd");
+  console.log("DEBUG[345]: url=", url);
   return (
     <div className="w-full max-w-7xl mx-auto p-4">
+      {/* <ReactPlayer */}
+      {/*   url="https://i.imgur.com/msw9Al2.mp4" */}
+      {/*   playing */}
+      {/*   loop */}
+      {/*   playsinline */}
+      {/*   muted */}
+      {/*   controls */}
+      {/*   onError={() => { */}
+      {/*     console.log("asdasd"); */}
+      {/*   }} */}
+      {/* /> */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {memes.map((meme) => (
           <Card key={meme.id}>
@@ -38,7 +52,9 @@ export default function MemesPage() {
                 />
               </div>
             </CardHeader>
-            <CardContent>{processValue(meme.value)}</CardContent>
+            <CardContent>
+              <Embed value={meme.value} />
+            </CardContent>
             <CardFooter>
               <div className="flex gap-2 justify-end grow">
                 <Button
@@ -63,33 +79,39 @@ export default function MemesPage() {
   );
 }
 
-function processValue(value: string): ReactNode {
-  const isUrl = value.startsWith("http://") || value.startsWith("https://");
+function Embed({ value }: { value: string }) {
+  let url;
+  try {
+    url = new URL(value);
+  } catch {
+    return <div>{value}</div>;
+  }
 
-  if (!isUrl) return value;
+  const pathnameSplit = url.pathname.split("/");
+  const lastPathname = pathnameSplit[pathnameSplit.length - 1];
 
-  const isImage = /\.(png|jpe?g|gif|webp|bmp|svg)(\?.*)?$/i.test(value);
+  const isImage = lastPathname
+    ? /\.(png|jpe?g|gif|webp|bmp|svg)(\?.*)?$/i.test(lastPathname)
+    : false;
 
   if (isImage) {
     const isDiscordCdn =
-      value.startsWith("https://cdn.discordapp.net/attachments") ||
-      value.startsWith("https://media.discordapp.net/attachments");
-
-    if (isDiscordCdn) return <ProxyCdnImage url={value} />;
-
-    return <img src={value} alt="Image" />;
-  } else {
-    return (
-      <a
-        href={value}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-blue-500"
-      >
-        {value}
-      </a>
-    );
+      url.hostname === "media.discordapp.net" ||
+      url.hostname === "cdn.discordapp.com";
+    if (isDiscordCdn) return <ProxyCdnImage url={url.href} />;
+    return <img src={url.href} alt="Image" />;
   }
+
+  return (
+    <a
+      href={value}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-blue-500"
+    >
+      {value}
+    </a>
+  );
 }
 
 function ProxyCdnImage({ url }: { url: string }) {
