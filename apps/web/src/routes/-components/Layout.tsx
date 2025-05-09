f0097ef8c22795a5f64b42df7052fa1ec0833215
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-05-09 12:15:27 +0700

    Adjust navbar

--- apps/web/src/routes/-components/Layout.tsx
+++ apps/web/src/routes/-components/Layout.tsx
@@ -11,14 +11,19 @@ export default function Layout({ children }: { children: ReactNode }) {
       <div className="h-16 bg-orange-200 flex justify-center border-b">
         <div className="max-w-7xl grow justify-end flex items-center px-4">
           {currentUser && (
-            <Avatar>
-              <AvatarImage
-                src={`https://cdn.discordapp.com/avatars/${currentUser?.id}/${currentUser?.avatar}`}
-              />
-              <AvatarFallback>
-                {currentUser?.username?.[0] ?? "A"}
-              </AvatarFallback>
-            </Avatar>
+            <div className="flex items-center gap-4">
+              <div className="text-primary-foreground">
+                {currentUser?.username}
+              </div>
+              <Avatar className="size-10">
+                <AvatarImage
+                  src={`https://cdn.discordapp.com/avatars/${currentUser?.id}/${currentUser?.avatar}`}
+                />
+                <AvatarFallback className="bg-primary-foreground text-primary">
+                  {currentUser?.username?.[0] ?? "A"}
+                </AvatarFallback>
+              </Avatar>
+            </div>
           )}
         </div>
       </div>
