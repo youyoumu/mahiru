77734c2cbf5fb37d4c516991cb3a11fe05a50e9e
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-05-09 13:23:34 +0700

    Create azusa pat

--- apps/web/src/routes/-components/HomePage.tsx
+++ apps/web/src/routes/-components/HomePage.tsx
@@ -1,5 +1,7 @@
 import { useCurrentUser } from "#/hooks/useCurrentUser";
 import { Link } from "@tanstack/react-router";
+import { useRef, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function HomePage() {
   const { data: currentUser, isLoading: L1 } = useCurrentUser();
@@ -19,7 +21,7 @@ export default function HomePage() {
         </div>
       </div>
       <div className="w-full max-w-7xl mx-auto p-4 flex flex-col items-center justify-center grow">
-        <img src="/azusa.webp" />
+        <LoveEmojiBubbles />
         {isNotLogin && (
           <div className="w-64 text-muted-foreground">
             You're not logged in. To get started, use the{" "}
@@ -31,3 +33,60 @@ export default function HomePage() {
     </div>
   );
 }
+
+const LoveEmojiBubbles = () => {
+  const parentRef = useRef<HTMLDivElement>(null);
+  const [emojis, setEmojis] = useState<
+    {
+      id: string;
+      x: number;
+      y: number;
+    }[]
+  >([]);
+
+  return (
+    <div
+      ref={parentRef}
+      className="relative group cursor-grab"
+      onMouseMove={(e) => {
+        const bounds = parentRef.current?.getBoundingClientRect();
+        if (!bounds) return;
+
+        const x = e.clientX - bounds.left;
+        const y = e.clientY - bounds.top;
+
+        if (y > 90) return;
+        if (x < 50) return;
+
+        const newEmoji = {
+          id: crypto.randomUUID(),
+          x: 0 + Math.random() * 200,
+          y: 0 + Math.random() * 50,
+        };
+        const randomNumber = Math.random();
+        if (randomNumber > 0.05) return;
+        setEmojis((prev) => [...prev, newEmoji]);
+        setTimeout(() => {
+          setEmojis((prev) => prev.filter((emoji) => emoji.id !== newEmoji.id));
+        }, 1000);
+      }}
+    >
+      <img src="/azusa.webp" className="size-64" />
+      <AnimatePresence>
+        {emojis.map((emoji) => (
+          <motion.div
+            key={emoji.id}
+            initial={{ opacity: 1, y: 0 }}
+            animate={{ opacity: 0, y: -50 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+            className="absolute text-3xl"
+            style={{ top: emoji.y, left: emoji.x }}
+          >
+            ❤️
+          </motion.div>
+        ))}
+      </AnimatePresence>
+    </div>
+  );
+};
