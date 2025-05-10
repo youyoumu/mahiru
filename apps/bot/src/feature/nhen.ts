db57cdaec1ae028d292e69dbe97f1bb208677d75
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-05-10 16:06:24 +0700

    Update embed successfully

--- apps/bot/src/feature/nhen.ts
+++ apps/bot/src/feature/nhen.ts
@@ -8,6 +8,7 @@ import {
   type PartialMessage,
 } from "discord.js";
 import { API, Book } from "nhentai-api";
+import { digits, number, pipe, safeParse, string, transform } from "valibot";
 
 const nH = new API();
 
@@ -27,7 +28,8 @@ export async function handleNhenLink({
   }
   const totalPages = book.pages.length;
   const page1 = book.pages[0] ? nH.getImageURL(book.pages[0]) : null;
-  const newPage1 = new URL(page1 ?? "");
+  if (!page1) return;
+  const newPage1 = new URL(page1);
   newPage1.hostname = "i4.nhentai.net";
 
   const embed = new EmbedBuilder()
@@ -53,10 +55,67 @@ export async function handleNhenLink({
   message.channel.send({ embeds: [embed], components: [row] });
 }
 
-export function handleNHenButtonInteraction({
+export async function handleNHenButtonInteraction({
   interaction,
 }: {
   interaction: ButtonInteraction;
 }) {
   console.log("DEBUG[429]: interaction=", interaction.message.embeds);
+
+  const str = interaction.message.embeds[0]?.footer?.text;
+  if (!str) return;
+  const [code, pageInfo] = str.split("-").map((part) => part.trim());
+  if (!pageInfo) return;
+  const [currentPage] = pageInfo.split("/");
+
+  const numberSchema = pipe(
+    string(),
+    digits(),
+    transform((s) => Number(s)),
+    number(),
+  );
+
+  const parsedCode = safeParse(numberSchema, code);
+  const parsedCurrentPage = safeParse(numberSchema, currentPage);
+  if (!parsedCode.success || !parsedCurrentPage.success) return;
+
+  let book: Book;
+  try {
+    book = await nH.getBook(parsedCode.output);
+  } catch {
+    return;
+  }
+  const totalPages = book.pages.length;
+
+  const index = parsedCurrentPage.output;
+  const page = book.pages[index] ? nH.getImageURL(book.pages[index]) : null;
+  if (!page) return;
+  const newPage = new URL(page);
+  newPage.hostname = "i4.nhentai.net";
+
+  console.log("Code:", code);
+  console.log("Current Page:", currentPage);
+  console.log("Total Page:", totalPages);
+
+  const embed = new EmbedBuilder()
+    .setTitle(book.title.pretty)
+    .setColor("#fef3c6")
+    .setFooter({
+      text: `${parsedCode.output} - ${index + 1}/${totalPages}`,
+    })
+    .setImage(newPage.toString());
+
+  const next = new ButtonBuilder()
+    .setCustomId("next")
+    .setEmoji("➡️")
+    .setStyle(ButtonStyle.Secondary);
+  const prev = new ButtonBuilder()
+    .setCustomId("prev")
+    .setEmoji("⬅️")
+    .setStyle(ButtonStyle.Secondary);
+  const row = new ActionRowBuilder<ButtonBuilder>()
+    .addComponents(prev)
+    .addComponents(next);
+
+  interaction.update({ embeds: [embed], components: [row] });
 }
