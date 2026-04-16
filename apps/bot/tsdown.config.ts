04f23bd11fc2f6610f7d85c99d3b38811b988fef
youyoumu <19760884+youyoumu@users.noreply.github.com>
2026-04-16 13:24:11 +0700

    build: bundler for docker

--- apps/bot/tsdown.config.ts
+++ apps/bot/tsdown.config.ts
@@ -1,11 +1,36 @@
+import { bundleStats } from "rollup-plugin-bundle-stats";
 import { defineConfig } from "tsdown";
 
-export default defineConfig({
-  entry: ["./src/index.ts"],
-  format: ["esm"],
-  dts: true,
-  sourcemap: true,
-  unbundle: true,
-  clean: false,
-  minify: false,
-});
+const isDocker = process.argv.includes("--on-docker");
+const config = {
+  default: defineConfig({
+    entry: ["./src/index.ts"],
+    format: ["esm"],
+    dts: true,
+    sourcemap: true,
+    unbundle: true,
+    clean: false,
+    minify: false,
+    plugins: [
+      bundleStats({
+        outDir: "..",
+      }),
+    ],
+  }),
+  docker: defineConfig({
+    entry: ["./src/index.ts"],
+    deps: {
+      onlyBundle: false,
+      alwaysBundle: [/.*/],
+      neverBundle: ["zlib-sync"],
+    },
+    format: ["esm"],
+    plugins: [
+      bundleStats({
+        outDir: "..",
+      }),
+    ],
+  }),
+};
+
+export default config[isDocker ? "docker" : "default"];
